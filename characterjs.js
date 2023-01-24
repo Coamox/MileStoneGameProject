c277b021f43bb7c0f20d0f1548c360182106659a
Coamox <coamox443@gmail.com>
2023-01-23 20:30:21 -0500

    New Pages Added and Local Storage test

--- characterjs.js
+++ characterjs.js
@@ -1,5 +1,14 @@
+
+
 document.getElementById('back').addEventListener('click', async (event) => 
 {
 	event.preventDefault()
 	window.location.href = "title.html";
+})
+
+document.getElementById('next').addEventListener('click', async (event) => 
+{
+	event.preventDefault()
+    window.localStorage.setItem("1","One");
+	window.location.href = "maingame.html";
 })
--- titlejs.js
+++ titlejs.js
@@ -2,5 +2,7 @@
 document.getElementById('start').addEventListener('click', async (event) => 
 {
 	event.preventDefault()
+    
 	window.location.href = "character.html";
+    
 })
